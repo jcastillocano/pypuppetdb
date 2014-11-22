68f09f316328708c3020d06fb4f37f23c91c6708
Juan Carlos <juan.carlos@tuguu.com>
2014-11-22 13:30:11 +0000

    Change urlpath to proxy url with authentication support

--- pypuppetdb/__init__.py
+++ pypuppetdb/__init__.py
@@ -72,8 +72,9 @@ except ImportError:  # pragma: notest
 logging.getLogger(__name__).addHandler(NullHandler())
 
 
-def connect(api_version=3, host='localhost', port=8080, ssl_verify=False,
-            ssl_key=None, ssl_cert=None, timeout=10):
+def connect(api_version=3, host='localhost', port=8080, urlproxy='',
+        username=None, password=None,
+        ssl_verify=False, ssl_key=None, ssl_cert=None, timeout=10):
     """Connect with PuppetDB. This will return an object allowing you
     to query the API through its methods.
 
@@ -85,6 +86,15 @@ def connect(api_version=3, host='localhost', port=8080, ssl_verify=False,
 
     :param port: (optional) Port on which to talk to PuppetDB.
     :type port: :obj:`int`
+
+    :param urlproxy: (optional) URL path for PuppetDB proxy (need slash).
+    :type urlproxy: :obj:`string`
+
+    :param username: (optional) user authentication for PuppetDB proxy.
+    :type username: :obj:`string`
+
+    :param password: (optional) password authentication for PuppetDB proxy.
+    :type password: :obj:`string`
 
     :param ssl: (optional) Talk with PuppetDB over SSL.
     :type ssl: :obj:`bool`
@@ -103,12 +113,12 @@ def connect(api_version=3, host='localhost', port=8080, ssl_verify=False,
     :raises: :class:`~pypuppetdb.errors.UnsupportedVersionError`
     """
     if api_version == 3:
-        return v3.API(host=host, port=port,
-                      timeout=timeout, ssl_verify=ssl_verify, ssl_key=ssl_key,
-                      ssl_cert=ssl_cert)
+        return v3.API(host=host, port=port, urlproxy=urlproxy, username=username,
+            password=password, timeout=timeout, ssl_verify=ssl_verify,
+            ssl_key=ssl_key, ssl_cert=ssl_cert)
     if api_version == 2:
-        return v2.API(host=host, port=port,
-                      timeout=timeout, ssl_verify=ssl_verify, ssl_key=ssl_key,
-                      ssl_cert=ssl_cert)
+        return v2.API(host=host, port=port, urlproxy=urlproxy, username=username,
+            password=password, timeout=timeout, ssl_verify=ssl_verify,
+            ssl_key=ssl_key, ssl_cert=ssl_cert)
     else:
         raise UnsupportedVersionError
